6c21b1a678850482c5c7265facc12bfd597c6c68
KarinaGordia <kgordya24@gmail.com>
2025-01-27 17:55:54 +0100

    emotions screen created

---
+++ lib/ui/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundLight = Color.fromRGBO(255, 253, 252, 1);
+  static const mandarin = Color.fromRGBO(255, 135, 2, 1);
+  static const black = Color.fromRGBO(76, 76, 105, 1);
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const grey2 = Color.fromRGBO(188, 188, 191, 1);
+  static const grey3 = Color.fromRGBO(145, 158, 171, 1);
+  static const grey4 = Color.fromRGBO(242, 242, 242, 1);
+  static const grey5 = Color.fromRGBO(225, 221, 216, 1);
+  static const mandarinTransparent = Color.fromRGBO(255, 135, 2, 0.25);
+  static const shadow = Color.fromRGBO(182, 161, 192, 0.11);
+}
---
+++ lib/ui/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mood_diary/ui/theme/app_colors.dart';
+
+final appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: AppColors.mandarin),
+  primaryColor: AppColors.mandarin,
+  scaffoldBackgroundColor: AppColors.backgroundLight,
+);
