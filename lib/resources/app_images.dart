27ba8cd6cbefcb895d88fd91ad33e22b670baa84
KarinaGordia <kgordya24@gmail.com>
2025-01-28 11:04:22 +0100

    preparing assets

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -2,4 +2,10 @@ class AppImages {
   AppImages._();
 
   static const String calendarIcon = 'assets/images/calendar_icon.svg';
+  static const String calmness = 'assets/images/calmness.png';
+  static const String fear = 'assets/images/fear.png';
+  static const String joy = 'assets/images/joy.png';
+  static const String rage = 'assets/images/rage.png';
+  static const String sadness = 'assets/images/sadness.png';
+  static const String strength = 'assets/images/strength.png';
 }
